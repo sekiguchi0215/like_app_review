73b042d3f437fb8191548e60547d86b7e8ae1f75
sekiguchi0215 <dsphere5296@gmail.com>
2021-08-06 01:34:54 +0900

    不備修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root "homes#index"
   resources :posts
   devise_scope :user do
-    post "users/guest_sign_in". to: "users/sessions#guest_sign_in"
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
 end
